0957fec5159ebc5d0f99d2a1a7eb45f82356c52d
Prochor Simon <prokhor.zimon@agilie.com>
2019-11-14 16:54:09 +0200

    update

---
+++ dist/KrakenApiFetch/index.d.ts
@@ -0,0 +1,17 @@
+import { SupPairs } from '../types/SupPairs';
+export default class KrakenApiFetch {
+    private KRAKEN_SOCKET_URL;
+    private readonly _socket;
+    private readonly _expectedPairs;
+    private _promiseResolve?;
+    private _promiseReject?;
+    private _getPrices;
+    constructor(...pairs: SupPairs[]);
+    fetch(): Promise<any>;
+    private _closeChannel;
+    private _subscribeForPrices;
+    private _handleMessage;
+    private _handleClose;
+    private _handleError;
+    private _isPriceValid;
+}
