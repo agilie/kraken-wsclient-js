21591b1e8815dea1d0f8743739e06f73da2fec25
prochor <prochorz@gmail.com>
2019-11-29 19:01:15 +0200

    remove dist

--- dist/KrakenClient/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Public, Private } from '../types/Methods';
-export default class KrakenClient {
-    private readonly url;
-    private readonly version;
-    api(method: Public | Private, params?: object): Promise<any>;
-    private publicMethod;
-    private privateMethod;
-    private request;
-}
